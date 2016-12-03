7146e240205a39dbb4061604c09fd2d19f4847c4
williamx <wxiang@stuy.edu>
2016-12-03 17:10:49 -0500

    kek

--- app.py
+++ app.py
@@ -14,13 +14,39 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.form.get("action") == "login":
+        user = request.form["user"]
+        pwd = request.form["pass"]
+        hashObj = hashlib.sha1()
+        hashObj.update(pwd)
+        pwd = hashObj.hexdigest()
+
+        if dbUtils.loginAuth(user, pwd) == 0:
+            session["user"] = user
+            return redirect(url_for("index"))
+        return render_template("auth.html", extra = "LOGIN INCORRECT")
+    else: #assert action == register
+        user = request.form["user"]
+        pwd = request.form["pass"]
+        confirm = request.form["confirm"]
+        pic = request.form["pic"]
+        if dbUtils.registerAuth(user, pwd, confirm) == 0:
+            hashObj = hashlib.sha1()
+            hashObj.update(pwd)
+            pwd = hashObj.hexdigest()
+            dbUtils.addUser(user, pwd, pic)
+            session["user"] = user #store session                                               
+            return redirect(url_for("index"))
+        return render_template("auth.html")
+
 
 @app.route('/<username>')
 def user(username):
-    return render_template("user.html")
+    return "kek"
+    #return render_template("user.html")
+
 
 @app.route('/kill')
 def kill():
