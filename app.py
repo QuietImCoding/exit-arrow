e9e21734c6c1607d574e55493baec1db2f305503
Daniel Monteagudo <dmonte3210@gmail.com>
2016-12-03 13:19:00 -0500

    Not a dinosaur

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ import json
 app = ClarifaiApp()
 url = raw_input("Pick a photo: ")
 files = [url]
-tags = ["dinosaur"]
+search = ["dinosaur"]
 d = app.tag_urls(files)
 
 for image in d["outputs"]:
     dinosaur = False
+    tags = []
     for concept in image["data"]["concepts"]:
-        if concept["name"].lower() in tags:
+        if concept["name"].lower() in search:
             dinosaur = True
+        else:
+            tags.append(concept["name"].lower())
     if dinosaur:
         print "DINOSAUR!!"
     else:
         print "NOT A DINOSAUR"
+        print tags
 
 
