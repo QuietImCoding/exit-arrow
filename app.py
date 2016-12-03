ffab1c91048459ffc75b7268cea5cb837910bf3b
williamx <wxiang@stuy.edu>
2016-12-03 16:38:58 -0500

    afawfwa

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ def index():
         return redirect( url_for('login') )
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 @app.route('/<username>')
 def user():
